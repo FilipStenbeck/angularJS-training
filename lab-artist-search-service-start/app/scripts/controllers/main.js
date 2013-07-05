cff7270e0218097a5020a278cfa342d1da242a05
Filip Stenbeck <filip.stenbeck@gmail.com>
2013-07-05 12:09:31 +0200

    Added artist images

--- lab-artist-search-service-start/app/scripts/controllers/main.js
+++ lab-artist-search-service-start/app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('formLab').controller('MainCtrl', function ($scope, $http) {
     //TODO Move this code to the 'musicService' Service
     $http({method: 'GET', url: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=cb54c2f3a327d9bd9a1ab3f6bd3d30e5&format=json'}).
       success(function(data, status, headers, config) {
-        $scope.artists = data.artists.artist;  
+          $scope.artists = data.artists.artist;  
       }).
       error(function(data, status, headers, config) {
       //Fallback if no network connection in lab room
