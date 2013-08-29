3af543469701bdd7a14c0c5aff5814447f30cd35
Filip Stenbeck <filip.stenbeck@gmail.com>
2013-08-29 12:37:58 +0200

    added new lab

--- lab-first-labs/watch/js/app.js
+++ lab-first-labs/watch/js/app.js
@@ -3,10 +3,12 @@ function helloController($scope) {
     $scope.helloModel = {
         message : 'Hello World!!'
     }
-    
+    $scope.state = "normal";
     $scope.counter = 0;
     $scope.$watch("helloModel.message", function(newValue, oldValue) {
-         $scope.counter++;  
+        if ($scope.counter++ > 8) {
+            $scope.state = "warning";  
+        };
     });
  
 }
