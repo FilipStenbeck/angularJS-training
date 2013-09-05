988193aec8eaa944345e31fafc28f0c6d5860c14
unknown <s61023@B5471295.corp1.ad1.seb.net>
2013-09-05 12:02:29 +0200

    Added Mediator demo

---
+++ lab-first-labs/mediator/app/js/app.js
@@ -0,0 +1,48 @@
+//The 'app' module uses the Mediator module 
+var app = angular.module("mediatorDemo", ['mediator']);
+
+app.controller("senderOneCtrl", function ($scope, mediator) {
+    //Install Mediator to local scope
+    mediator.installTo($scope);
+    
+    //Handle button click
+    $scope.send = function () { 
+        $scope.publish("oneSending");
+    };
+});
+
+app.controller("senderTwoCtrl", function ($scope, mediator) {
+    //Install Mediator to local scope
+    mediator.installTo($scope);
+    
+    //Handle button click
+    $scope.send = function () {
+        $scope.publish("twoSending");
+    };
+});
+
+app.controller("recieverCtrl", function ($scope, mediator) {
+    //Install Mediator to local scope
+    mediator.installTo($scope);
+    
+    //Create Reciever Model
+    $scope.reciever = {
+        message : '',
+        total : 0
+    }
+    
+    //Declare events handler
+    var handleMessage = function(msg) {
+        $scope.reciever.total++;
+        $scope.reciever.message = msg;
+    };
+    
+    //Subscribe to Events
+    $scope.subscribe("oneSending",  function() {
+        handleMessage('Recieved message from sender One');
+    });
+    $scope.subscribe("twoSending", function() {
+        handleMessage('Recieved message from sender Two');
+    });
+});
+
