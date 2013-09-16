c9809be90ae1e966677e2a4ebb485544bad3f94f
unknown <s61023@B5471295.corp1.ad1.seb.net>
2013-09-16 13:26:39 +0200

    added artisty list to form lab

--- labs/lab-signup-form-start/app/scripts/controllers/main.js
+++ labs/lab-signup-form-start/app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
-angular.module('formLab').controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
+angular.module('formLab').controller('MainCtrl', function ($scope, musicService) {
+    
+    musicService.getTopArtist(function(data) {
+        $scope.artists = data;   
+    });
+   
 });
