4bc5cfd20ca941b2a6334b09d72ca426e571deea
unknown <s61023@B5471295.corp1.ad1.seb.net>
2013-09-20 09:35:09 +0200

    moved finished labs

--- labs/routing/app/scripts/directives/message.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('routingApp')
-  .directive('message', function () {
-    return {
-      templateUrl: 'views/messageTemplate.html',
-      restrict: 'E'
-    };
-  });
