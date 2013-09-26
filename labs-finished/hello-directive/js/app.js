72e904ab5e018fd841e5d9d6d84c7f302fdce492
unknown <s61023@B5471295.corp1.ad1.seb.net>
2013-09-26 09:22:12 +0200

    added behavior to the directive

--- labs-finished/hello-directive/js/app.js
+++ labs-finished/hello-directive/js/app.js
@@ -4,14 +4,22 @@ app.directive("helloDirective", function () {
     return {
         restrict: "E",
         link: function postLink(scope, element, attributes) {
-            var message, big;
-            message = document.createTextNode(attributes.messsage);
+            var big;
+         
             if (attributes.format === 'big') {
                 big = document.createElement("h1");
-                big.appendChild(message);
+                big.textContent =  attributes.messsage;;
                 element[0].appendChild(big);
             } else {
-                element[0].appendChild(message);
+                element[0].textContent =  attributes.messsage;;
+            }
+			
+			 element[0].onclick = function (event) {
+                 if (attributes.format === 'big') {
+                    big.textContent = 'I was clicked'
+                 } else {
+                      element[0].textContent = 'I was also clicked'
+                 }
             }
         }
     };
